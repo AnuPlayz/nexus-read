38c01b37c0308ba2addc97174c2cb5cb89fedc4a
Aniruddh <adubge@gmail.com>
2024-01-09 20:55:25 +0530

    a

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,5 +10,10 @@ export default {
       },
     },
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   plugins: [],
 } satisfies Config;
